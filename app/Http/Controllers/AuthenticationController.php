8418b17944ba8833c3c08bfd82e35001f2b7dce5
Syam Apriansyah <86736888+empampom@users.noreply.github.com>
2026-02-02 14:49:40 +0700

    Fix formatting and update redirect target in login functionality

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -2,30 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Procurement;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class AuthenticationController extends Controller
 {
     public function login()
     {
-        return view("login");
+        return view('login');
     }
 
     public function LoginAct(Request $request)
     {
         $validated = $request->validate([
-            "username" => "required|min:3|max:100",
-            "password" => "required|min:3|max:100",
+            'username' => 'required|min:3|max:100',
+            'password' => 'required|min:3|max:100',
         ]);
         if (Auth::attempt($validated)) {
             $request->session()->regenerate();
-            return redirect()->intended('dashboard');
+
+            return redirect()->intended('administrator');
         }
+
         return back()->withErrors([
             'username' => 'The provided credentials do not match our records.',
         ])->onlyInput('username');
