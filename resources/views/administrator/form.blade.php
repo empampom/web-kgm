b4408ad9c5654bbad4be4fb27f30a4ea7dcc195e
mzfa <zulfikarfebri1505@gmail.com>
2026-02-03 12:22:30 +0700

    EDIT -- ganti textarea plugin

--- resources/views/administrator/form.blade.php
+++ resources/views/administrator/form.blade.php
@@ -7,7 +7,8 @@
     <title>{{ env('APP_NAME') }}</title>
     <link href="{{ asset('assets/img/logo.png') }}" rel="icon" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/froala-editor@latest/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
+    {{-- <link href="https://cdn.jsdelivr.net/npm/froala-editor@latest/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" /> --}}
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.20/dist/summernote-lite.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -62,7 +63,10 @@
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/froala-editor@latest/js/froala_editor.pkgd.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.20/dist/summernote-lite.min.js"></script>
+
+    {{-- <script src="https://cdn.jsdelivr.net/npm/froala-editor@latest/js/froala_editor.pkgd.min.js"></script>
     <script>
         var editor = new FroalaEditor("#content", {
             toolbarButtons: [
@@ -71,7 +75,21 @@
                 ['formatOLSimple', 'formatUL', 'insertLink'],
             ]
         });
+    </script> --}}
+
+    <script>
+        $(document).ready(function() {
+            $('#content').summernote({
+                height: 300,
+                toolbar: [
+                    ['style', ['bold', 'italic', 'underline']],
+                    ['insert', ['picture', 'link']],
+                    ['para', ['ul', 'ol', 'paragraph']],
+                ]
+            });
+        });
     </script>
+
 </body>
 
 </html>
